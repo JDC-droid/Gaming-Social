ebd324c5ea20ca1c4af090383709f1da97114cf3
JDC-droid <josh040890@gmail.com>
2025-05-19 09:22:59 +0100

    add structure code of main pagejs

--- src/app/components/PostComponent.js
+++ src/app/components/PostComponent.js
@@ -3,14 +3,14 @@ import React from "react";
 const PostComponent = ({ post }) => {
   return (
     <div
-      class={
+      className={
         post
           ? "bg-[#603f8b] pt-4 pl-4 pr-4 pb-25 rounded-md border-2 border-[#323b36]"
           : "hidden"
       }
     >
-      <h1>{title}</h1>
-      <p>{description}</p>
+      <h1>{post.title}</h1>
+      <p>{post.description}</p>
     </div>
   );
 };
--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,61 @@
 "use client";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import React from "react";
+import PostComponent from "./components/PostComponent";
 
 export default function Home() {
-  return <div></div>;
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const loadPosts = () => {
+      const localStoragePosts = localStorage.getItem("posts");
+      if (localStoragePosts) {
+        const parsedPosts = JSON.parse(localStoragePosts);
+        setPosts(parsedPosts);
+        setLoading(false);
+      }
+    };
+    loadPosts();
+  }, []);
+
+  const handleNewPost = (id) => {
+    console.log("New Post ID: ", id);
+    const postUpdate = posts.find((post) => post.id === id);
+    if (postUpdate) {
+      setPosts((prevPosts) =>
+        prevPosts.map((post) => (post.id === id ? postUpdate : post))
+      );
+      return postUpdate;
+    }
+    localStorage.setItem("posts", JSON.stringify(posts));
+    setPosts(posts);
+  };
+
+  if (loading) {
+    return (
+      <div>
+        <h1>Loading...</h1>
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      {posts?.length === 0 ? (
+        <h1>No posts available</h1>
+      ) : (
+        posts.map((post) => (
+          <PostComponent
+            key={post.id}
+            post={post}
+            title={post.title}
+            description={post.description}
+            id={post.id}
+            handleNewPost={handleNewPost}
+          />
+        ))
+      )}
+    </div>
+  );
 }
